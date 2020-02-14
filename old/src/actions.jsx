82a5068215eaf7f200f87499d8eb4daf6d76606f
Unknown <inga.fuhrmann@gmail.com>
2020-02-14 14:09:05 +0100

    delete

--- old/src/actions.jsx
+++
@@ -1,7 +0,0 @@
-export const SET_CURRENT_RED = "SET_CURRENT_RED";
-export const truee = "true";
-export const setCurrentBarRed = i => ({ type: SET_CURRENT_RED, index: i, key: truee});
-
-export const SET_CURRENT_BLUE = "SET_CURRENT_BLUE";
-export const falsee = "false";
-export const setCurrentBarBlue = i => ({ type: SET_CURRENT_BLUE, index: i, key: falsee});
